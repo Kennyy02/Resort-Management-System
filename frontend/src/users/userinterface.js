8a821fbb27d4c00fa622216a361e0702e05d64d0
Kennyy02 <136206628+Kennyy02@users.noreply.github.com>
2025-11-26 21:35:45 +0800

    Update userinterface.js

--- frontend/src/users/userinterface.js
+++ frontend/src/users/userinterface.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import Feedbacks from './Feedbacks'; 
+import Feedbacks from './feedbacks'; 
 import './styles/homepage.css'; 
 
 // Placeholder image (create this file in your project)
